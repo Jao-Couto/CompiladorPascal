4e937b52b77a476cd71a1186040553094dc6091a
João Couto <73503848+Jao-Couto@users.noreply.github.com>
2022-10-04 13:30:09 -0300

    interface

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+const { lexer } = require("./src/lexer")
+
+function analyze() {
+    let program = document.getElementById("codeArea").value
+    console.log(program);
+    lexer.setInput(program)
+    let result = ''
+    while ((match = lexer.lex()) != 1) {
+        result += match
+        console.log(match)
+    }
+    document.getElementById("lexerArea").value = result;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
+    let fileInput = document.getElementById("fileUpload");
+    fileInput.addEventListener("change", () => {
+        let file = fileInput.files[0];
+        if (file) {
+            var reader = new FileReader();
+            reader.readAsText(file, "UTF-8");
+            reader.onload = function (evt) {
+                document.getElementById("analyze").disabled = false;
+                document.getElementById("codeArea").value = evt.target.result;
+            }
+            reader.onerror = function (evt) {
+                document.getElementById("codeArea").value = "Erro ao ler o arquivo";
+            }
+        }
+    })
+
+
+
+
+
+});
 
