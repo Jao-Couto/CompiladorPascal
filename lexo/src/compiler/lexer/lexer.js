32a42d176d9e2e7407b7563045a833cffba32e86
Joao Couto <joao.vitor1229@hotmail.com>
2023-01-05 21:35:33 -0300

    escrevendo BNF

--- lexo/src/compiler/lexer/lexer.js
+++ lexo/src/compiler/lexer/lexer.js
@@ -7,11 +7,24 @@ const lexemes = {
         "INVALID_IDENTIFIER": "[A-Za-z_][A-Za-z_0-9]{15,}",
         "VALID_IDENTIFIER": "[A-Za-z_][A-Za-z_0-9]{0,14}",
         "LINE_FEED": "(\\r|\\n|\\r\\n)",
-        "TYPES": "(Integer|Real|Character|Boolean)"
+        "TYPES": "(int|boolean)"
     },
     rules: [
         // Palavras reservadas
         ["program", "return ['PROGRAM', yytext, [yylloc.first_line, yylloc.first_column]];"],
+        ["var", "return ['VAR', yytext, [yylloc.first_line, yylloc.first_column]];"],
+        ["{TYPES}", "return ['TYPE', yytext, [yylloc.first_line, yylloc.first_column]];"],
+        ["begin", "return ['BEGIN', yytext, [yylloc.first_line, yylloc.first_column]];"],
+        ["end", "return ['END', yytext, [yylloc.first_line, yylloc.first_column]];"],
+        ["procedure", "return ['PROCEDURE', yytext, [yylloc.first_line, yylloc.first_column]];"],
+
+        // Operadores
+        [":=", "return ['ASSIGN', yytext, [yylloc.first_line, yylloc.first_column]];"],
+        ["(<>|<=|<|>=|>|=)", "return ['RELATIONAL_OP', yytext, [yylloc.first_line, yylloc.first_column]];"],
+        ["(\\*|div|and)", "return ['ARITMETHIC_OP_FACTOR', yytext, [yylloc.first_line, yylloc.first_column]];"],
+        ["(\\+,\\-)", "return ['ARITMETHIC_OP_SIMPLE', yytext, [yylloc.first_line, yylloc.first_column]];"],
+        ["or", "return ['OR', yytext, [yylloc.first_line, yylloc.first_column]];"],
+        ["not", "return ['NOT', yytext, [yylloc.first_line, yylloc.first_column]];"],
 
         // Identificadores e números
         ["{INVALID_IDENTIFIER}", "return ['INVALID_ID_LENGTH', yytext, [yylloc.first_line, yylloc.first_column]];"],
@@ -20,8 +33,9 @@ const lexemes = {
         ["{DIGIT}{1,8}", "return ['NUMBER', yytext, [yylloc.first_line, yylloc.first_column]];"],
 
         // Delimitadores
-        [",", "return ['COLON', null, [yylloc.first_line, yylloc.first_column]];"],
+        [",", "return ['COLON', ',', [yylloc.first_line, yylloc.first_column]];"],
         [";", "return ['SEMICOLON', ';', [yylloc.first_line, yylloc.first_column]];"],
+        ["\\.", "return ['DOT', '.', [yylloc.first_line, yylloc.first_column]];"],
         [" ", "return;"],
         ["\t", "return;"],
         ["{LINE_FEED}+", "return;"],
@@ -32,16 +46,13 @@ const lexemes = {
         ["\\\{(\\*(?!\/)|[^*])*", "return ['UNEXPECTED_EOF', null, [yylloc.first_line, yylloc.first_column]];"],
 
         // Parênteses
-        ["\\(", "return ['LPAREN', null, [yylloc.first_line, yylloc.first_column]]"],
-        ["\\)", "return ['RPAREN', null, [yylloc.first_line, yylloc.first_column]]"],
+        ["\\(", "return ['LPAREN', '(', [yylloc.first_line, yylloc.first_column]]"],
+        ["\\)", "return ['RPAREN', ')', [yylloc.first_line, yylloc.first_column]]"],
 
-        // Operadores
-        [":=", "return ['ASSIGN', null, [yylloc.first_line, yylloc.first_column]];"],
-        ["(<>|<=|<|>=|>|=)", "return ['RELATIONAL_OP', yytext, [yylloc.first_line, yylloc.first_column]];"],
-        ["(\\*|\\+|\\-|\\/|%)", "return ['ARITMETHIC_OP', yytext, [yylloc.first_line, yylloc.first_column]];"],
+
 
         // Declaração de tipo
-        [":", "return ['TYPE_DECLARATION', null, [yylloc.first_line, yylloc.first_column]];"],
+        [":", "return ['TYPE_DECLARATION', ':', [yylloc.first_line, yylloc.first_column]];"],
 
         // Final de arquivo
         ["\\.", "return ['EOP', null, [yylloc.first_line, yylloc.first_column]];"],
